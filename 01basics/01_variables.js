d10135c9b1cdb0d1b3ad4643134fe9137c033c67
Shubham-Baheti <shubhambaheti187@gmail.com>
2024-11-02 18:48:47 +0530

    This is my day1 to learn javascript variables

---
+++ 01basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =14453
+let accountEmail ="shubhambh@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId=2
+ console.log(accountId);
+ console.table([accountEmail,accountId,accountCity,accountState])
+ 
